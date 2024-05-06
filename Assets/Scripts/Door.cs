7348bcec96d9a6b02a20fab924f4a8617bbb336a
rookiePuppet <1617384798@qq.com>
2024-05-06 18:06:48 +0800

    添加瓦片碰撞体层；增加房门动画

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Door : MonoBehaviour
+{
+    private Animator animator;
+
+    private static readonly int AnimatorHash_Open = Animator.StringToHash("Open");
+    private static readonly int AnimatorHash_Close = Animator.StringToHash("Close");
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        animator.SetTrigger(AnimatorHash_Open);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        animator.SetTrigger(AnimatorHash_Close);
+    }
+}
